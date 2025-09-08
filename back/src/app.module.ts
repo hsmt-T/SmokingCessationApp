19a4ae0eb974ba15481581ad5216cbcc38eaa41f
hsmtDP-T <ittuyo.hashi244@gmail.com>
2025-09-08 17:31:09 +0900

    ユーザーモジュールを追加し、JWT認証を実装しました。関連するDTO、サービス、コントローラーを作成しました。また、必要なパッケージを追加しました。

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,7 +11,8 @@ import { PrismaModule } from './prisma/prisma.module';
       envFilePath: '.env', // .env を全体で使えるようにする
     }),
     PrismaModule,
-    AuthModule, // ← ここで認証モジュールを読み込み
+    AuthModule,
+    UserModule, // ← ここで認証モジュールを読み込み
   ],
 })
 export class AppModule {}
